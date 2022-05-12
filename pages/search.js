84f5e29893e153c801afc763690333302b31ce0a
lejt <92820402+lejt@users.noreply.github.com>
2022-05-12 15:01:42 -0700

    setting search default params and fetch for search page

--- pages/search.js
+++ pages/search.js
@@ -4,9 +4,13 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
-import SearchFilters from '../components/SearchFIlters';
 
-function search() {
+import SearchFilters from '../components/SearchFIlters';
+import Property from '../components/Property';
+import noresult from '../assets/images/noresult.svg';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
+function search({ properties }) {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -22,14 +26,49 @@ function search() {
                 fontSize="lg"
                 justifyContent="center"
                 alignItems="center"
-                onClick={() => setSearchFilter((prevFilters) => !prevFilters)}
+                onClick={() => setSearchFilters((prevFilters) => !prevFilters)}
             >
                 <Text>Search Property By Filters</Text>
                 <Icon paddingLeft="2" w="7" as={BsFilter} />
             </Flex>
             {searchFilters && <SearchFilters />}
+            <Text fontSize="2xl" fontWeight="bold">
+                Properties {router.query.purpose}
+            </Text>
+            <Flex flexWrap='wrap'>
+                {properties.map((property => <Property property={property} key={property.id}/>))}
+            </Flex>
+            {properties.length === 0 && (
+                <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
+                    <Image alt="no result" src={noresult} />
+                    <Text fontSize="2xl" marginTop="3">No Results Found</Text>
+                </Flex>
+            )}
         </Box>
     )
 }
 
 export default search
+
+// NextJS has two forms of pre-rendering: (1) Static Generation, (2) Server-side Rendering
+// 
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || 'for-rent';
+  const rentFrequency = query.rentFrequency || 'yearly';
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+
+  const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
